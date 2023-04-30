cabaf96fa6b81b66af55e464ba1101c15692a37d
Wdrm <126005544+Wdrm@users.noreply.github.com>
2023-04-30 15:31:10 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -20,15 +19,47 @@ public class Main {
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+
+       // System.out.println(curso1);
+       // System.out.println(curso2);
+       // System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+
+
+        Dev devWellington = new Dev();
+        devWellington.setNome("Wellington");
+        devWellington.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos "+ devWellington.getConteudosInscritos());
+        devWellington.progredir();
+        System.out.println("__");
+        System.out.println("Conteúdos Inscritos "+ devWellington.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos "+ devWellington.getConteudosConcluidos());
+      //  System.out.println("XP:"+devWellington.calculaTotalXP());
+
+
+        System.out.println("________________");
+
+
+        Dev devAnna = new Dev();
+        devAnna.setNome("Anna");
+        devAnna.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos "+ devAnna.getConteudosInscritos());
+        devAnna.progredir();
+        System.out.println("__");
+        System.out.println("Conteúdos Inscritos "+ devAnna.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos "+ devAnna.getConteudosConcluidos());
+        //System.out.println("XP:"+devAnna.calculaTotalXP());
 
 
 
 
-
-
+        }
 
     }
-}
